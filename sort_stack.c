f89111f9fa6619bcd8400dae89a5556746d156fe
Furiosso <dgimenodemanuel@gmail.com>
2024-06-11 16:08:51 +0200

    new sorting method started

--- sort_stack.c
+++ sort_stack.c
@@ -4,8 +4,10 @@
 void	sort_stack(t_stack **stack_a);
 char	check_order(t_stack *stack);
 //void	radix_sort(stack_a, &stack_b);
+void	compare_sort(t_stack **stack_a, t_stack **stack_b);
 void	bubble_sort(t_stack **stack_a, t_stack **stack_b);
 void	custom_sort(t_stack **stack_a, t_stack **stack_b);
+int	find_middle(t_stack *stack);
 size_t	list_size(t_stack *stack);
 t_stack	*last_node(t_stack *stack);
 
@@ -24,7 +26,8 @@ void	sort_stack(t_stack **stack_a)
 		aux = aux->next;
 	}
 	printf("\n");*/
-	custom_sort(stack_a, &stack_b);
+	//custom_sort(stack_a, &stack_b);
+	compare_sort(stack_a, &stack_b);
 	//radix_sort(stack_a, &stack_b);
 	//bubble_sort(stack_a, &stack_b);
 	//swap("sa", stack_a);
@@ -43,6 +46,68 @@ void	radix_sort(stack_a, &stack_b)
 {
 
 }*/
+
+void	compare_sort(t_stack **stack_a, t_stack **stack_b)
+{
+	size_t	size;
+	int	middle;
+	//t_stack	*aux;
+
+	middle = find_middle(*stack_a);
+	//printf("middle: %d\n", middle);
+	size = list_size(*stack_a);
+	//printf("size: %zu\n", size);
+	while (size--)
+	{
+		if ((*stack_a)->number < middle)
+		{
+			push_stack("pb", stack_b, stack_a);
+			if (*stack_b && (*stack_b)->next && (*stack_b)->number < (*stack_b)->next->number)
+				swap("sb", stack_b);
+		}
+		else
+			rotate("ra", stack_a);
+	}
+	/*aux = *stack_b;
+	printf("size stack_b: %zu\n", list_size(*stack_b));
+	printf("size stack_a: %zu\n", list_size(*stack_a));
+	printf("Stack b: ");
+	while (aux)
+	{
+		printf(" %d |", aux->number);
+		aux = aux->next;
+	}
+	printf("\n");*/
+}
+
+int	find_middle(t_stack *stack)
+{
+	size_t	len;
+	size_t 	con;
+	t_stack	*aux;
+	t_stack	*aux2;
+
+	len = list_size(stack);
+	//printf("Size: %zu\n", len);
+	aux = stack;
+	while (aux)
+	{
+		aux2 = stack;
+		con = 0;
+		while (aux2)
+		{
+			if (aux->number < aux2->number)
+				con++;
+			aux2 = aux2->next;
+		}
+		//printf("Con: %zu\n", con);
+		if (con == len / 2)
+			break ;
+		aux = aux->next;
+	}
+	//printf("Con: %zu\n", con);
+	return (aux->number + 1);
+}
 
 void	custom_sort(t_stack **stack_a, t_stack **stack_b)
 {
